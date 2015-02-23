2c5bb6e5cb7e159f960d572ee14565387df5d0bc
Carl Capozza <carlcapozza@gmail.com>
2015-02-23 17:08:53 -0500

    WIP, still working on forms and edit and delete links at this point

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,15 +1,35 @@
 class GreetingsController < ApplicationController
+
 	def show
   	@greeting = Greetings.find(params[:id])
-  	respond_to do |format|
-  		format.html
-  		format.json { render json: @greeting }
-  	end
+    respond_to do |format|
+      format.html
+      format.json { render json: @greeting }
+    end
   end
+
   def index
   	@greeting = Greetings.all
   	respond_to do |format|
   		format.html
+      format.json { render json: @greeting }
   	end
   end
+
+  def edit
+  
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
