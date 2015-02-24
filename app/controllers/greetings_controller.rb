f5416ca5cf659d652d012459c63a2e0458c78f2d
Carl Capozza <carlcapozza@gmail.com>
2015-02-24 17:04:23 -0500

    WIP Incremental.  Figured out the delete.  Still working on the object updating.

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,7 +1,12 @@
 class GreetingsController < ApplicationController
+  before_action :get_greeting, only: [:show, :destroy]
+
+  def get_greeting
+    @greeting = Greetings.find(params[:id])
+  end
+
 
 	def show
-  	@greeting = Greetings.find(params[:id])
     respond_to do |format|
       format.html
       format.json { render json: @greeting }
@@ -17,19 +22,26 @@ class GreetingsController < ApplicationController
   end
 
   def edit
-  
+    @greeting = Greetings.find(params[:id])
   end
+
+  def update
+    @greeting = Greetings.find(params[:id])
+    if @greeting.update_attributes(params[:greeting])
+      flash[:notice] = "You have updated this greeting"
+    else
+      render("edit")
+    end
+  end
+
 
   def create
 
   end
 
-  def update
-
-  end
-
   def destroy
-
+    @greeting.destroy
+    redirect_to :action => 'index', :format => 'html'
   end
 
 end
